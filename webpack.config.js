05b4efb972d078bbd8ecdaeccb435b05c14af4c4
Yufan Fei <yufafei@amazon.com>
2018-07-20 16:51:26 -0700

    Initialize the boilerplate for developing react project
    - Add eslintrc to support airbnb-eslint standard
    - Add webpack.config.js to support hot reloading with web-dev-server
    - Add babelrc to support ES6 and jsx transpile

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: ['./src/index.jsx'],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-react', '@babel/preset-env', '@babel/preset-stage-1'],
+      },
+    }],
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000,
+    },
+  },
+};
