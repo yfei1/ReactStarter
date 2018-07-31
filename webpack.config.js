54e7b9d43d4fc7189c8978ea7b2f60eee419f3fe
Yufan Fei <yfei816@gmail.com>
2018-07-31 00:13:54 -0700

    Fix babel 7.0 backward incompatibility and add webpack-dev-server settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
   entry: ['./src/index.jsx'],
   output: {
@@ -6,24 +8,37 @@ module.exports = {
     filename: 'bundle.js',
   },
   module: {
+    // apply the following babel rules to files with .js or .jsx extensions
     rules: [{
       test: /\.(js|jsx)$/,
       exclude: /node_modules/,
       loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-react', '@babel/preset-env', '@babel/preset-stage-1'],
+        presets: ['@babel/react', '@babel/env'],
+        plugins: [
+          '@babel/transform-arrow-functions',
+          '@babel/proposal-object-rest-spread',
+          '@babel/transform-template-literals',
+        ],
       },
     }],
   },
+  // without adding file types to resolve will lead to compile time import erors
+  // e.g. import App from './App' -> App.js not found
   resolve: {
-    extensions: ["*", ".webpack.js", ".web.js", ".js", ".json", ".jsx"]
+    extensions: ['*', '.webpack.js', '.web.js', '.js', '.json', '.jsx'],
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: './',
+    hot: true,
+    open: true,
+    watchContentBase: true,
     watchOptions: {
       aggregateTimeout: 300,
-      poll: 1000,
+      poll: 3000,
     },
   },
+plugins: [
+  new webpack.HotModuleReplacementPlugin()
+]
 };
